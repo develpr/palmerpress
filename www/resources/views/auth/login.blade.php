66d80e8baa8006bd4947d9e46dc4344f74132879
Kevin Mitchell <kevin@develpr.com>
2016-01-20 23:52:53 -0600

    adding issues, tweaks to templates to start making them real

--- www/resources/views/auth/login.blade.php
+++ www/resources/views/auth/login.blade.php
@@ -3,64 +3,34 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {!! csrf_field() !!}
+            @if($errors->has('password') || $errors->has('email'))
+            <div class="callout alert">
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">E-Mail Address</label>
+                <p>Email and password combination was not found. Please try again.</p>
 
-                            <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input type="password" class="form-control" name="password">
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-sign-in"></i>Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
             </div>
+            @endif
+            <form class="" role="form" method="POST" action="{{ url('/login') }}">
+                {!! csrf_field() !!}
+                <div class="medium-6 columns">
+                    <label class="{{ $errors->has('email') ? ' is-invalid-label' : '' }}">Email
+                        <input type="text" class="{{ $errors->has('email') ? ' is-invalid-input' : '' }}" placeholder="Email" value="{{ old('email') }}">
+                    </label>
+                </div>
+                <div class="medium-6 columns">
+                    <label class="{{ $errors->has('password') ? ' is-invalid-label' : '' }}">Password
+                        <input type="password" class="{{ $errors->has('password') ? ' is-invalid-input' : '' }}">
+                    </label>
+                </div>
+
+                <fieldset class="large-12 columns">
+                    <input id="remember" type="checkbox"><label for="remember">Remember Me</label>
+                </fieldset>
+
+                <div class="large-12">
+                <button type="submit" class="button float-center large-4 small-10">Save</button>
+                </div>
+            </form>
         </div>
     </div>
-</div>
 @endsection
